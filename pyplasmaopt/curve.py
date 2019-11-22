2e6b1150b40b311334e5b9f8b37ada6410f239d8
Florian Wechsung <f.wechsung@gmail.com>
2019-11-22 15:17:02 -0500

    added magnetic axis

--- pyplasmaopt/curve.py
+++ pyplasmaopt/curve.py
@@ -54,6 +54,23 @@ class Curve():
             )/denominator**6
         return res
 
+    def plot(self, resolution=100, ax=None, show=True, plot_derivative=False):
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d import Axes3D
+
+        phis = np.linspace(0, 1, resolution)
+        gammas = np.stack(self.gamma(phis))
+        if ax is None:
+            fig = plt.figure()
+            ax = fig.gca(projection='3d')
+        ax.plot(gammas[:, 0], gammas[:, 1], gammas[:, 2])
+        if plot_derivative:
+            dgamma_by_dphi = self.dgamma_by_dphi(phis)[:, 0, :]
+            ax.quiver(gammas[:, 0], gammas[:, 1], gammas[:, 2], 0.1 * dgamma_by_dphi[:, 0], 0.1 * dgamma_by_dphi[:, 1], 0.1 * dgamma_by_dphi[:, 2], arrow_length_ratio=0.1, color="r")
+        if show:
+            plt.show()
+        return ax
+
 
 class CartesianFourierCurve(Curve):
     r"""
@@ -138,19 +155,116 @@ with
         return res
 
 
-    def plot(self, resolution=100, ax=None, show=True, plot_derivative=False):
-        import matplotlib.pyplot as plt
-        from mpl_toolkits.mplot3d import Axes3D
 
-        phis = np.linspace(0, 2*pi, resolution)
-        gammas = np.stack(self.gamma(phis))
-        if ax is None:
-            fig = plt.figure()
-            ax = fig.gca(projection='3d')
-        ax.plot(gammas[:, 0], gammas[:, 1], gammas[:, 2])
-        if plot_derivative:
-            dgamma_by_dphi = np.stack(self.dgamma_by_dphi(phis))
-            ax.quiver(gammas[:, 0], gammas[:, 1], gammas[:, 2], 0.1 * dgamma_by_dphi[:, 0], 0.1 * dgamma_by_dphi[:, 1], 0.1 * dgamma_by_dphi[:, 2], arrow_length_ratio=0.1, color="r")
-        if show:
-            plt.show()
-        return ax
+class StelleratorSymmetricCylindricalFourierCurve(Curve):
+
+    def __init__(self, order, nfp):
+        self.coefficients = [np.zeros((order+1,)), np.zeros((order,))]
+        self.nfp = nfp
+        self.order = order
+
+    def num_coeff(self):
+        return 2*self.order+1
+
+    def get_dofs(self):
+        return np.concatenate(self.coefficients)
+
+    def set_dofs(self, dofs):
+        counter = 0
+        for i in range(self.order+1):
+            self.coefficients[0][i] = dofs[i]
+        for i in range(self.order):
+            self.coefficients[1][i] = dofs[self.order + 1 + i]
+
+    def gamma(self, points):
+        res = np.zeros((len(points), 3))
+        nfp = self.nfp
+        for i in range(self.order+1):
+            res[:, 0] += self.coefficients[0][i] * np.cos(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+            res[:, 1] += self.coefficients[0][i] * np.cos(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+        for i in range(1, self.order+1):
+            res[:, 2] += self.coefficients[1][i-1] * np.sin(nfp * 2 * pi * i * points)
+        return res
+
+    def dgamma_by_dcoeff(self, points):
+        res = np.zeros((len(points), self.num_coeff(), 3))
+        nfp = self.nfp
+        for i in range(self.order+1):
+            res[:, i, 0] = np.cos(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+            res[:, i, 1] = np.cos(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+        for i in range(1, self.order+1):
+            res[:, self.order + i, 2] = np.sin(nfp * 2 * pi * i * points)
+        return res
+
+    def dgamma_by_dphi(self, points):
+        res = np.zeros((len(points), 1, 3))
+        nfp = self.nfp
+        for i in range(self.order+1):
+            res[:, 0, 0] += self.coefficients[0][i] * (
+                -(nfp * 2 * pi * i) * np.sin(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+                -(2 * pi) *           np.cos(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+            )
+            res[:, 0, 1] += self.coefficients[0][i] * (
+                -(nfp * 2 * pi * i) * np.sin(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+                +(2 * pi) *           np.cos(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+            )
+        for i in range(1, self.order+1):
+            res[:, 0, 2] += self.coefficients[1][i-1] * (nfp * 2 * pi * i) * np.cos(nfp * 2 * pi * i * points)
+        return res
+
+    def d2gamma_by_dphidcoeff(self, points):
+        res = np.zeros((len(points), 1, self.num_coeff(), 3))
+        nfp = self.nfp
+        for i in range(self.order+1):
+            res[:, 0, i, 0] = (
+                -(nfp * 2 * pi * i) * np.sin(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+                -(2 * pi) *           np.cos(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+            )
+            res[:, 0, i, 1] = (
+                -(nfp * 2 * pi * i) * np.sin(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+                +(2 * pi) *           np.cos(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+            )
+        for i in range(1, self.order+1):
+            res[:, 0, self.order + i, 2] = (nfp * 2 * pi * i) * np.cos(nfp * 2 * pi * i * points)
+        return res
+
+    def d2gamma_by_dphidphi(self, points):
+        res = np.zeros((len(points), 1, 1, 3))
+        coeffs = self.coefficients
+        nfp = self.nfp
+        for i in range(self.order+1):
+            res[:, 0, 0, 0] += self.coefficients[0][i] * (
+                -(nfp * 2 * pi * i)**2 *       np.cos(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+                +(nfp * 2 * pi * i)*(2 * pi) * np.sin(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+                +(nfp * 2 * pi * i)*(2 * pi) * np.sin(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+                -(2 * pi)**2 *                 np.cos(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+            )
+            res[:, 0, 0, 1] += self.coefficients[0][i] * (
+                -(nfp * 2 * pi * i)**2 *         np.cos(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+                -(nfp * 2 * pi * i) * (2 * pi) * np.sin(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+                -(2 * pi) * (nfp * 2 * pi * i) * np.sin(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+                -(2 * pi)**2 *                   np.cos(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+            )
+        for i in range(1, self.order+1):
+            res[:, 0, 0, 2] -= self.coefficients[1][i-1] * (nfp * 2 * pi * i)**2 * np.sin(nfp * 2 * pi * i * points)
+        return res
+
+    def d3gamma_by_dphidphidcoeff(self, points):
+        res = np.zeros((len(points), 1, 1, self.num_coeff(), 3))
+        nfp = self.nfp
+        for i in range(self.order+1):
+            res[:, 0, 0, i, 0] = (
+                -(nfp * 2 * pi * i)**2 *       np.cos(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+                +(nfp * 2 * pi * i)*(2 * pi) * np.sin(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+                +(nfp * 2 * pi * i)*(2 * pi) * np.sin(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+                -(2 * pi)**2 *                 np.cos(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+            )
+            res[:, 0, 0, i, 1] = (
+                -(nfp * 2 * pi * i)**2 *         np.cos(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+                -(nfp * 2 * pi * i) * (2 * pi) * np.sin(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+                -(2 * pi) * (nfp * 2 * pi * i) * np.sin(nfp * 2 * pi * i * points) * np.cos(2 * pi * points)
+                -(2 * pi)**2 *                   np.cos(nfp * 2 * pi * i * points) * np.sin(2 * pi * points)
+            )
+        for i in range(1, self.order+1):
+            res[:, 0, 0, self.order + i, 2] = -(nfp * 2 * pi * i)**2 * np.sin(nfp * 2 * pi * i * points)
+        return res
--- tests/test_curve.py
+++ tests/test_curve.py
@@ -4,44 +4,45 @@ import pytest
 def taylor_test(f, df, x, epsilons=None, direction=None):
     f0 = f(x)
     if direction is None:
-        direction = np.random.rand(*(x.shape))
+        direction = np.random.rand(*(x.shape))-0.5
     dfx = direction@df(x)
     if epsilons is None:
-        epsilons = np.power(2., -np.asarray(range(5, 10)))
+        epsilons = np.power(2., -np.asarray(range(10, 15)))
     print("################################################################################")
     err_old = 1e9
     for eps in epsilons:
         feps = f(x + eps * direction)
         dfest = (feps-f0)/eps
         err = np.linalg.norm(dfest - dfx)
-        assert err < 1e-12 or err < 0.6 * err_old
+        assert err < 1e-11 or err < 0.6 * err_old
         err_old = err
         print(err)
     print("################################################################################")
 
 
 
-def get_curve():
+def get_coil():
     from pyplasmaopt import CartesianFourierCurve
 
     cfc = CartesianFourierCurve(3)
-    cfc.coefficients[1][1] = 1.
-    cfc.coefficients[2][2] = 1.
+    cfc.coefficients[1][0] = 1.
+    cfc.coefficients[1][1] = 0.5
+    cfc.coefficients[2][2] = 0.5
     return cfc
 
-def test_check_first_derivative():
-    cfc = get_curve()
+def test_coil_first_derivative():
+    cfc = get_coil()
     x = np.asarray([1])
     taylor_test(lambda p: cfc.gamma(p), lambda p: cfc.dgamma_by_dphi(p), x)
 
-def test_dof_numbering():
-    cfc = get_curve()
+def test_coil_dof_numbering():
+    cfc = get_coil()
     coeffs = cfc.get_dofs()
     cfc.set_dofs(coeffs)
     assert(np.allclose(coeffs, cfc.get_dofs()))
 
-def test_coefficient_derivative():
-    cfc = get_curve()
+def test_coil_coefficient_derivative():
+    cfc = get_coil()
     coeffs = cfc.get_dofs()
     x = np.asarray([1])
     def f(dofs):
@@ -52,10 +53,10 @@ def test_coefficient_derivative():
         return cfc.dgamma_by_dcoeff(x)
     taylor_test(f, df, coeffs)
 
-def test_curvature_derivative():
+def test_coil_curvature_derivative():
     # This implicitly also tests the higher order derivatives of gamma as these
     # are needed to compute the derivative of the curvature.
-    cfc = get_curve()
+    cfc = get_coil()
     coeffs = cfc.get_dofs()
     x = np.asarray([1])
     def f(dofs):
@@ -66,8 +67,56 @@ def test_curvature_derivative():
         return cfc.dkappa_by_dcoeff(x)
     taylor_test(f, df, coeffs)
 
+def get_magnetic_axis():
+    from pyplasmaopt import StelleratorSymmetricCylindricalFourierCurve
+
+    ma = StelleratorSymmetricCylindricalFourierCurve(3, 3)
+    ma.coefficients[0][0] = 1.
+    ma.coefficients[0][1] = 0.1
+    ma.coefficients[1][0] = 0.1
+    return ma
+
+def test_magnetic_axis_first_derivative():
+    ma = get_magnetic_axis()
+    x = np.asarray([1])
+    taylor_test(lambda p: ma.gamma(p), lambda p: ma.dgamma_by_dphi(p), x)
+
+def test_magnetic_axis_dof_numbering():
+    ma = get_magnetic_axis()
+    coeffs = ma.get_dofs()
+    ma.set_dofs(coeffs)
+    assert(np.allclose(coeffs, ma.get_dofs()))
+
+def test_magnetic_axis_coefficient_derivative():
+    ma = get_magnetic_axis()
+    coeffs = ma.get_dofs()
+    x = np.asarray([1])
+    def f(dofs):
+        ma.set_dofs(dofs)
+        return ma.gamma(x)
+    def df(dofs):
+        ma.set_dofs(dofs)
+        return ma.dgamma_by_dcoeff(x)
+    taylor_test(f, df, coeffs)
+
+def test_magnetic_axis_curvature_derivative():
+    # This implicitly also tests the higher order derivatives of gamma as these
+    # are needed to compute the derivative of the curvature.
+    ma = get_magnetic_axis()
+    coeffs = ma.get_dofs()
+    x = np.asarray([1])
+    def f(dofs):
+        ma.set_dofs(dofs)
+        return ma.kappa(x)
+    def df(dofs):
+        ma.set_dofs(dofs)
+        return ma.dkappa_by_dcoeff(x)
+    taylor_test(f, df, coeffs)
 
 if __name__ == "__main__":
-    cfc = get_curve()
-    cfc.plot(plot_derivative=True)
+    cfc = get_coil()
+    ax = cfc.plot(plot_derivative=True, show=False)
+    ma = get_magnetic_axis()
+    ax = ma.plot(ax=ax, plot_derivative=False)
+
 

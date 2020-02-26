9164f93f3fbc63c44dbd058e203a5655d22dbf26
Florian Wechsung <f.wechsung@gmail.com>
2020-02-26 11:20:24 -0500

    100 outsamples

--- examples/obfgsruns.sh
+++ examples/obfgsruns.sh
@@ -4,8 +4,8 @@ for c in 0.1 0.3 1.0; do
     for lam in 0 1e-7 1e-5 1e-3; do
         for lr in 0.1 0.15 0.2; do
             for tau in 10 100 1000; do
-                opts="--mode stochastic --sigma 0.003 --length-scale 0.2 --ninsamples 20 --noutsamples 1000 --optimizer sgd --ppp 20 --c $c --lam $lam --lr $lr --tau $tau"
-                echo "python3 example2.py $opts; python3 example2_plot.py $opts" >> obfgstasks.txt
+                opts="--mode stochastic --sigma 0.003 --length-scale 0.2 --ninsamples 20 --noutsamples 100 --optimizer sgd --ppp 20 --c $c --lam $lam --lr $lr --tau $tau"
+                echo "python3.7 example2.py $opts; python3.7 example2_plot.py $opts" >> obfgstasks.txt
             done
         done
     done

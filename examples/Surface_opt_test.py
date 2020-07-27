0a0969002f95c990ec94d85c6e82fad2fbf7d632
jlball <jlball813@gmail.com>
2020-07-27 15:52:00 -0400

    First version working with analytic jacobian. New initial guess and coil / currents

--- examples/Surface_opt_test.py
+++ examples/Surface_opt_test.py
@@ -9,22 +9,22 @@ from generate_toroidal_guess import toroidal_guess
 
 def main():
 	nfp = 2
-	N = 20
+	N = 25
 
 	#Imports coil data
-	(coils, ma) = get_matt_data(nfp=2, ppp=20, at_optimum=True)
+	(coils, ma) = get_matt_data(nfp=2, ppp=25, at_optimum=True)
 
 	#Currents given by Andrew
-	currents = np.array([-219001.804742907, -215024.346505962, -212678.904950201, -213524.933314340, -205978.356488100, -198316.662919875])
+	currents = np.array([-206907.1130006409, -193961.5148358337, -203645.2797680261, -206078.7868965321, -233742.9534465412, -220188.9284706694])
 	eta_bar = -2.105800979374183
 
 	cc = CoilCollection(coils, currents, nfp, True)
 
 	surface_meta = s.Surface(N, N, nfp)
 
-	X = np.loadtxt("x_coord.txt", delimiter=',')
-	Y = np.loadtxt("y_coord.txt", delimiter=',')
-	Z = np.loadtxt("z_coord.txt", delimiter=',')
+	X = np.loadtxt("x_to_john.dat", delimiter=',')
+	Y = np.loadtxt("y_to_john.dat", delimiter=',')
+	Z = np.loadtxt("z_to_john.dat", delimiter=',')
 
 	sa_target = 7.895683520871486
 
@@ -40,38 +40,15 @@ def main():
 	#import ipdb; ipdb.set_trace(context=21)
 	#Flattening all of the surface information and rotational transform into one very long 1-D array
 	x = np.concatenate((X_flat, Y_flat, Z_flat, np.array([iota_init])))
-
-	def iter_newton(X, imax = 1e6,tol = 1e-5):
-   		for i in range(int(imax)):
-   			Y, J = compute_boozer(X, surface_meta, cc, sa_target)
-   			print(np.amax(np.abs(Y)))
-   			res_prev = np.amax(np.abs(Y))
-   			dX = np.linalg.solve(J,Y) # solve for increment from JdX = Y 
-   			X_p = X - dX # step X by dX 
-   			
-   			Y_p, J_p = compute_boozer(X_p, surface_meta, cc, sa_target)
-   			
-   			while np.amax(np.abs(Y_p)) > res_prev:
-   				dX = dX/2
-   				X_p = X-dX 
-   				
-   				Y_p, J_p = compute_boozer(X_p, surface_meta, cc, sa_target)
-   				
-   			X = X_p
-   			
-   			if np.linalg.norm(dX)<tol: # break if converged
-   				print('converged.')
-   				break
-   				
-   		return X 
-
-	sol = iter_newton(x)
 	
-	return
+	sol = optimize.root(compute_boozer, x, args=(surface_meta, cc, sa_target), jac=True) 
 
+	print("Optimization successful?", sol.success)
 
 	#Reshape optimize.root output to be able to plot
 	total_points = surface_meta.np_theta * surface_meta.np_varphi
+
+	np.savetxt("py_out.txt", sol.x)
 
 	x_sol = np.zeros(total_points)
 	y_sol = np.zeros(total_points)

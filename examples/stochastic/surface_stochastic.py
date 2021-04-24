4eeeed7e1178b18799eae8bc2797000d72096619
Florian Wechsung <f.wechsung@gmail.com>
2021-04-23 22:56:25 -0400

    continuation in noise magnitude

--- examples/stochastic/surface_stochastic.py
+++ examples/stochastic/surface_stochastic.py
@@ -15,6 +15,7 @@ def plot(s, filename):
     mlab.view(azimuth=45, elevation=45, distance=8)
     mlab.savefig(filename, magnification=4)
     mlab.close()
+    mlab.options.offscreen = False
 
 import argparse
 
@@ -50,7 +51,7 @@ x = np.load(outdir + "xiterates.npy")[it, :]
 print(f'||dJ||={np.load(outdir + "dJvals.npy")[it, 0]}')
 obj.outdir = outdir
 obj.update(x)
-savedir = outdir + "/surfaces/"
+savedir = outdir + "/surfaces2/"
 import shutil
 shutil.rmtree(savedir, ignore_errors=True)
 import os
@@ -70,137 +71,104 @@ bs = BiotSavart(coils, currents)
 
 nfp = 3
 stellsym = True
-ntor = 5
+ntor = 10
 mpol = 5
 nphi = (2-stellsym)*ntor + 1
 ntheta = 2*mpol + 1
-
-# for plotting
-phisp = np.linspace(0, 1, 2*nfp*nphi, endpoint=True)
-thetasp = np.linspace(0, 1, 4*ntheta, endpoint=True)
-sp = SurfaceXYZTensorFourier(mpol=mpol, ntor=ntor, nfp=nfp, stellsym=stellsym, quadpoints_phi=phisp, quadpoints_theta=thetasp)
 
 phis = np.linspace(0, 1/((1+stellsym)*nfp), nphi, endpoint=False)
 thetas = np.linspace(0, 1.0, ntheta, endpoint=False)
 
 
 sold = None
-surfaces = []
+surfaces_is = []
 allres = []
 
-nsurfaces = 20 if forplotting else 4
+nsurfaces = 20 if forplotting else 2
 target_areas = np.asarray([1.0 + i*1.0 for i in range(nsurfaces)])
+# target_fluxes = np.asarray([((i+1)*2.650e-02)**2 for i in range(nsurfaces)])
+l = 0.0167
 for i in range(nsurfaces):
     s = SurfaceXYZTensorFourier(mpol=mpol, ntor=ntor, nfp=nfp, stellsym=stellsym, quadpoints_phi=phis, quadpoints_theta=thetas)
     if sold is None:
-        s.fit_to_curve(ma, 0.0167, flip_theta=True)
+        s.fit_to_curve(ma, l, flip_theta=True)
     else:
         s.set_dofs(sold.get_dofs())
-        s.extend_via_normal(-0.0167)
+        s.extend_via_normal(-l)
 
+    tf = ToroidalFlux(s, bs_tf)
     ar = Area(s)
     # ar_target = ar.J()
     print('Area', ar.J())
     ar_target = target_areas[i]
     boozer_surface = BoozerSurface(bs, s, ar, ar_target) 
-    tf = ToroidalFlux(s, bs_tf)
-    tf_target = tf.J()
     # boozer_surface = BoozerSurface(bs, s, tf, tf_target) 
 
     # compute surface first using LBFGS exact and an area constraint
     if i == 0:
         res = boozer_surface.minimize_boozer_penalty_constraints_LBFGS(tol=1e-10, maxiter=3000, constraint_weight=100., iota=iota, G=G)
-        print(f"iota={res['iota']:.3f}, tf={tf.J():.3e}, area={ar.J():.3f}, ||residual||={np.linalg.norm(boozer_surface_residual(s, res['iota'], res['G'], bs, derivatives=0)):.3e}")
+        print(f"iota={res['iota']:.3f}, sqrt(tf)={np.sqrt(tf.J()):.3e}, area={ar.J():.3f}, ||residual||={np.linalg.norm(boozer_surface_residual(s, res['iota'], res['G'], bs, derivatives=0)):.3e}")
     # s.plot(show=True)
     # else:
     #     s, iota, _ = boozer_surface.minimize_boozer_penalty_constraints_LBFGS(tol=1e-10, maxiter=300, constraint_weight=100., iota=iota)
     #     print(f"iota={iota:.3f}, tf={tf.J():.3f}, area={ar.J():.3f}, ||residual||={np.linalg.norm(boozer_surface_residual(s, iota, bs, derivatives=0)):.3e}")
     # s.plot()
     res = boozer_surface.minimize_boozer_penalty_constraints_ls(tol=1e-10, maxiter=100, constraint_weight=100., iota=res['iota'], G=res['G'], method='manual')
-    print(f"iota={res['iota']:.3f}, tf={tf.J():.3e}, area={ar.J():.3f}, ||residual||={np.linalg.norm(boozer_surface_residual(s, res['iota'], res['G'], bs, derivatives=0)):.3e}")
+    print(f"iota={res['iota']:.3f}, sqrt(tf)={np.sqrt(tf.J()):.3e}, area={ar.J():.3f}, ||residual||={np.linalg.norm(boozer_surface_residual(s, res['iota'], res['G'], bs, derivatives=0)):.3e}")
     print(s.gamma()[0, 0, :])
-    surfaces.append(s)
+    surfaces_is.append(s)
     allres.append(res)
     sold = s
 
-surfaces_full = []
 surfaces_oos = []
-surfaces_is = []
-phis_full = np.linspace(0, 1, 2*(nfp*ntor)+1+15, endpoint=false)
-thetas_full = np.linspace(0, 1, 2*(mpol)+1+5, endpoint=false)
-phis_full = np.linspace(0, 1, 2*(nfp*ntor)+1 + 10, endpoint=false)
+surfaces_oos_full = []
+surfaces_is_full = []
+# phis_full = np.linspace(0, 1, 2*(nfp*ntor)+1, endpoint=false)
+# thetas_full = np.linspace(0, 1, 2*(mpol)+1, endpoint=false)
+phis_full = np.linspace(0, 1, 2*(nfp*ntor)+1, endpoint=false)
 thetas_full = np.linspace(0, 1, 2*(mpol)+1, endpoint=false)
-for s in surfaces:
-    surface_initial_guess = res['s']
-    sfull = SurfaceXYZTensorFourier(
-        mpol=mpol, ntor=ntor, nfp=nfp, stellsym=True, quadpoints_phi=phis_full, quadpoints_theta=thetas_full)
-    sfull.set_dofs(s.get_dofs())
-    surfaces_full.append(sfull)
-    surfaces_oos.append(
-        SurfaceXYZTensorFourier(
-            mpol=mpol, ntor=nfp*ntor, nfp=1, stellsym=False, clamped_dims=[False, True, False], quadpoints_phi=phis_full, quadpoints_theta=thetas_full))
-    surfaces_is.append(
-        SurfaceXYZTensorFourier(
-            mpol=mpol, ntor=nfp*ntor, nfp=1, stellsym=False, clamped_dims=[False, True, False], quadpoints_phi=phis_full, quadpoints_theta=thetas_full))
+for i in range(nsurfaces):
+    surfaces_oos.append(SurfaceXYZTensorFourier(
+        mpol=mpol, ntor=ntor, nfp=nfp, stellsym=stellsym, quadpoints_phi=phis, quadpoints_theta=thetas))
+
+    surfaces_oos_full.append(SurfaceXYZTensorFourier(
+        mpol=mpol, ntor=nfp*ntor, nfp=1, stellsym=False, clamped_dims=[False, True, False], quadpoints_phi=phis_full, quadpoints_theta=thetas_full))
+    surfaces_is_full.append(SurfaceXYZTensorFourier(
+        mpol=mpol, ntor=nfp*ntor, nfp=1, stellsym=False, clamped_dims=[False, True, False], quadpoints_phi=phis_full, quadpoints_theta=thetas_full))
 
 
 
 
-# s.plot()
+swork = SurfaceXYZTensorFourier(
+    mpol=mpol, ntor=ntor, nfp=nfp, stellsym=True, quadpoints_phi=phis_full, quadpoints_theta=thetas_full)
+sfullwork = SurfaceXYZTensorFourier(
+        mpol=mpol, ntor=nfp*ntor, nfp=1, stellsym=False, clamped_dims=[False, True, False], quadpoints_phi=phis_full, quadpoints_theta=thetas_full)
 
-# import sys; sys.exit()
-
-# surfaces_to_plot = [surfaces[i] for i in range(0, len(surfaces), 4)]
-# for s in surfaces_to_plot:
-#     # sp = s
-#     sp.set_dofs(s.get_dofs())
-#     xyz = sp.gamma()
-#     n = sp.normal()
-#     n = n/np.linalg.norm(n, axis=2)[..., None]
-#     B = bs.set_points(xyz.reshape((xyz.shape[0]*xyz.shape[1], 3))).B().reshape(xyz.shape)
-#     Bn = np.sum(B*n, axis=2)
-#     print(np.max(np.abs(Bn)))
-#     sp.plot(show=False, scalars=Bn)
-# for c in coils:
-#     c.plot_mayavi(show=False)
-# mlab.colorbar()
-# mlab.show()
-
-# def magnetic_field_on_surface(s):
-#     sp.set_dofs(s.get_dofs())
-#     xyz = sp.gamma()
-#     B = bs.set_points(xyz.reshape((xyz.shape[0]*xyz.shape[1], 3))).B().reshape(xyz.shape)
-#     mod_B = np.linalg.norm(B, axis=2)
-#     return mod_B
-
-# fig, axes = plt.subplots(nrows=len(surfaces_to_plot)//2, ncols=2)
-# for i, s in enumerate(surfaces_to_plot):
-#     print(i)
-#     B = magnetic_field_on_surface(s)
-#     plt.subplot(len(surfaces_to_plot)//2, 2, i+1)
-#     im = plt.contourf(sp.quadpoints_phi, sp.quadpoints_theta, B.T)#, levels=levels)
-#     plt.colorbar()
-# plt.show()
+def partial_to_full(spartial, sfull):
+    swork.set_dofs(spartial.get_dofs())
+    sfullwork.least_squares_fit(swork.gamma())
+    sfull.set_dofs(sfullwork.get_dofs())
 
 
-def compute_surface_for_perturbed_coils(bs_pert, res, s_ig, s, ar_target, bfgs_first=False):
-    s.least_squares_fit(s_ig.gamma())
+def compute_surface(bs_pert, res, s, ar_target, bfgs_first=0, exact=False):
     ar = Area(s)
+    # bs_flux = BiotSavart(bs_pert.coils, bs_pert.coil_currents)
     boozer_surface = BoozerSurface(bs_pert, s, ar, ar_target) 
-    if bfgs_first:
+    if bfgs_first > 0:
         res = boozer_surface.minimize_boozer_penalty_constraints_LBFGS(
-            tol=1e-10, maxiter=1000, constraint_weight=100., iota=res['iota'], G=res['G'])
+            tol=1e-10, maxiter=bfgs_first, constraint_weight=100., iota=res['iota'], G=res['G'])
         print(f"iota={res['iota']:.10f}, tf={ToroidalFlux(s, bs_pert).J():.3e}, area={ar.J():.3f}, ||residual||={np.linalg.norm(boozer_surface_residual(s, res['iota'], res['G'], bs_pert, derivatives=0)):.3e}, ||gradient||={np.linalg.norm(res['gradient']):.3e}")
-    # res = boozer_surface.minimize_boozer_penalty_constraints_ls(
-    #     tol=1e-10, maxiter=100, constraint_weight=100., iota=res['iota'], G=res['G'])
-    # print(f"iota={res['iota']:.10f}, area={ar.J():.3f}, ||residual||={np.linalg.norm(boozer_surface_residual(s, res['iota'], res['G'], bs_pert, derivatives=0)):.3e}")
     res = boozer_surface.minimize_boozer_penalty_constraints_ls(
-        tol=1e-9, maxiter=200, constraint_weight=1e2, iota=res['iota'], G=res['G'], method='manual', linear_solver='svd')
+        tol=1e-10, maxiter=100, constraint_weight=1e2, iota=res['iota'], G=res['G'], method='manual', linear_solver='lu', lam=1e-4)
     print(f"iota={res['iota']:.10f}, tf={ToroidalFlux(s, bs_pert).J():.3e}, area={ar.J():.3f}, ||residual||={np.linalg.norm(boozer_surface_residual(s, res['iota'], res['G'], bs_pert, derivatives=0)):.3e}, ||gradient||={np.linalg.norm(res['gradient']):.3e}, iter={res['iter']}")
-    # res = boozer_surface.solve_residual_equation_exactly_newton(iota=res['iota'], G=res['G'])
-    # print(f"iota={res['iota']:.10f}, area={ar.J():.3f}, ||residual||={np.linalg.norm(boozer_surface_residual(s, res['iota'], res['G'], bs_pert, derivatives=0)):.3e}")
-    if np.linalg.norm(res['gradient']) > 1e-8:
-        raise RuntimeError('norm of gradient too large')
+    if exact:
+        res = boozer_surface.solve_residual_equation_exactly_newton(iota=res['iota'], G=res['G'])
+        print(f"iota={res['iota']:.10f}, tf={ToroidalFlux(s, bs_pert).J():.3e}, area={ar.J():.3f}, ||residual||={np.linalg.norm(boozer_surface_residual(s, res['iota'], res['G'], bs_pert, derivatives=0)):.3e}")
+        if np.linalg.norm(res['residual']) > 1e-9:
+            raise RuntimeError('norm of residual too large')
+    else:
+        if np.linalg.norm(res['gradient']) > 1e-8:
+            raise RuntimeError('norm of gradient too large')
     return res
 
 
@@ -234,9 +202,9 @@ def compute_non_quasisymmetry_L2(s, bs):
 # print(non_qs)
 res_is = []
 for i in range(nsurfaces):
+    partial_to_full(surfaces_is[i], surfaces_is_full[i])
     try:
-        res, s_ig, s, ar_target = allres[i], surfaces_full[i], surfaces_is[i], target_areas[i]
-        res_is.append(compute_surface_for_perturbed_coils(bs, res, s_ig, s, ar_target))
+        res_is.append(compute_surface(bs, allres[i], surfaces_is_full[i], target_areas[i]))
         plot(s, savedir + f"plot_is_{i}.png")
     except Exception as ex:
         print(ex)
@@ -282,34 +250,46 @@ for i in range(N):
     madata_pert = find_magnetic_axis(bs_pert, ma_points, np.linalg.norm(ma.gamma()[0, 0:2]), output='cartesian')
     ma_pert = CurveRZFourier(ma_points, ma.order, 1, False)
     ma_pert.least_squares_fit(madata_pert)
+    sample_origs = [[s.copy() for s in c.sample] for c in bs_pert.coils]
+    ncont = 10
+    alphas = np.linspace(0, 1., ncont, endpoint=True)
     for j in range(nsurfaces_success):
+        print(f"Surface {j}")
+        # if j == 0:
+        #     s = surfaces_oos[j]
+        #     s.fit_to_curve(ma, l, flip_theta=True)
+        # else:
+        #     s = surfaces_oos[j]
+        #     s.set_dofs(surfaces_oos[j-1].get_dofs())
+        #     s.extend_via_normal(-l)
         try:
-            mapoints_fine = np.zeros((len(phis_full), 3))
-            ma.gamma_impl(mapoints_fine, phis_full)
-            mapertpoints_fine = np.zeros((len(phis_full), 3))
-            ma_pert.gamma_impl(mapertpoints_fine, phis_full)
+            # res_oos = compute_surface(bs_pert, res_is[j], s, target_areas[j], bfgs_first=1000 if (j==0) else 20, exact=False)
+            # partial_to_full(s, sfull)
+            # print(sfull.gamma()[0, 0, :])
             if j == 0:
-                s_ig = surfaces_oos[j]
-                target = surfaces_is[j].gamma()
-                target = target + (mapertpoints_fine-mapoints_fine)[:, None, :]
-                # s_ig.fit_to_curve(ma_pert, 0.01, flip_theta=True)
-                s_ig.least_squares_fit(target)
+                sfull = surfaces_oos_full[j]
+                res_oos = res_is[j]
+                sfull.set_dofs(surfaces_is_full[j].get_dofs())
+                for k in range(ncont):
+                    for ci in range(len(bs_pert.coils)):
+                        bs_pert.coils[ci].sample = [alphas[k] * s for s in sample_origs[ci]]
+                        bs_pert.coils[ci].invalidate_cache()
+                    res_oos = compute_surface(bs_pert, res_oos, sfull, target_areas[j], bfgs_first=False, exact=True)
             else:
-                s_ig = surfaces_is[j]
-            # s_ig = surfaces_is[j]
-            # import IPython; IPython.embed()
-            # import sys; sys.exit()
-            res_oos = compute_surface_for_perturbed_coils(bs_pert, res_is[j], s_ig, surfaces_oos[j], target_areas[j], bfgs_first=(j==0))
+                s = surfaces_oos[j]
+                s.set_dofs(surfaces_oos[j-1].get_dofs())
+                s.extend_via_normal(-l)
+                res_oos = compute_surface(bs_pert, res_oos, sfull, target_areas[j], bfgs_first=20, exact=False)
             oos_non_qs_L2[j, i], oos_qs_L2[j, i] = compute_non_quasisymmetry_L2(res_oos['s'], bs_pert)
             oos_non_qs_l2[j, i], oos_qs_l2[j, i] = compute_non_quasisymmetry_l2(res_oos['s'], bs_pert)
             oos_iotas[j, i] = res_oos['iota']
             plot(surfaces_oos[j], savedir + f"plot_oos_{j}_{i}.png")
         except Exception as ex:
             print(ex)
-            continue
+            break
 
 
-np.set_printoptions(edgeitems=30, linewidth=200, formatter=dict(float=lambda x: "%.15e" % x))
+np.set_printoptions(edgeitems=30, linewidth=200, formatter=dict(float=lambda x: "%.5e" % x))
 print("Non-QS    ", is_non_qs_L2)
 print("Non-QS OOS", np.mean(oos_non_qs_L2, axis=1))
 print("Non-QS OOS", oos_non_qs_L2)
